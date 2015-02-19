f849397e1725652d9b09852b0d6522a8d805baf1
Abby Mullen <abby@lincolnmullen.com>
2015-02-19 11:14:10 -0500

    Program to split texts and pull out metadata

--- text_cleaning_BW.py
+++ text_cleaning_BW.py
@@ -1,7 +1,7 @@
 import re
 
 
-#f = open("v1.txt", 'r')
+f = open("v1.txt", 'r')
 text = open('test.txt')
 a = text.readlines()
 p = "".join(a)
@@ -11,40 +11,52 @@ docbreaks = docbreak.split("DOCBREAK")
 def snippetyielder(docbreaks):
 	for doc in docbreaks:
 		yield doc
-			# elif re.match(r"(.*NDA.*)",line):
-			# 	print line	
-			# elif re.match(r"(.*NR\&L.*)",line):
-			# 	print line
-			# elif re.match(r"(.Am\. State Paper.*)",line):
-			# 	print line
-			# elif re.match(r"(.*\[Statutes.*)",line):
-			# 	print line
-			# elif re.match(r"(.*NYPL.*)",line):
-			# 	print line
-			# elif re.match(r"(.*\[Treaties.*)",line):
-			# 	print line	
-			# elif re.match(r"(.*\[LC.*)",line):
-			# 	print line
-			# elif re.match(r"(.*\[GAO.*)",line):
-			# 	print line
-	#print line
-				#print line
+# 			# elif re.match(r"(.*NDA.*)",line):
+# 			# 	print line	
+# 			# elif re.match(r"(.*NR\&L.*)",line):
+# 			# 	print line
+# 			# elif re.match(r"(.Am\. State Paper.*)",line):
+# 			# 	print line
+# 			# elif re.match(r"(.*\[Statutes.*)",line):
+# 			# 	print line
+# 			# elif re.match(r"(.*NYPL.*)",line):
+# 			# 	print line
+# 			# elif re.match(r"(.*\[Treaties.*)",line):
+# 			# 	print line	
+# 			# elif re.match(r"(.*\[LC.*)",line):
+# 			# 	print line
+# 			# elif re.match(r"(.*\[GAO.*)",line):
+# 			# 	print line
+# 	#print line
+# 				#print line
+
+t = snippetyielder(docbreaks)
+
+txt = open('snippets.txt', 'w')
+txt.write(t)
+
+#testdoc = open('testdoc.txt', 'r')
+
+# class Document(object):
+# 	def __init__(self, doc):
+# 		self.doc = doc
+
+# 	def raw_text(self, doc):
+
+# 		doc = doc.readlines()
+# 		j = "".join(doc)
+# 		raw_text = re.sub(r"\f.*[0-9]+",r"",j) #using formfeed to get rid of some page numbers/running heads
+# 		raw_text = re.sub(r"NAVAL OP.*",r"",j) #eliminating more headers
+#  		raw_text = re.sub(r"W.*B.*",r"",j) #eliminating more headers
+#  		print raw_text
 
 
-#snippetyielder()
+# def metadata:
 
- class document:
-  	def _init_(self, doc):
-  		self.doc = doc
+# def get_date:
 
-#  	def raw_text:
+# def does_this_look_suspicious:
 
-# 	def metadata:
+trial_doc = Document(t)
 
-# 	def get_date:
-
-# 	def does_this_look_suspicious:
-		
-
-
-
+print trial_doc.raw_text(trial_doc)
