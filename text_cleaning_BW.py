8ed266199295a42c64eea2d1c481abde16ce62ec
Abby Mullen <abby@lincolnmullen.com>
2015-02-21 11:05:25 -0500

    Before implementing Ben's changes

--- text_cleaning_BW.py
+++ text_cleaning_BW.py
@@ -5,44 +5,49 @@ f = open("v1.txt", 'r')
 text = open('test.txt')
 a = text.readlines()
 p = "".join(a)
+
+#detecting the breaks between documents and identifying them to break the docs with
 docbreak = re.sub(r"(.*SDA.*)",r"\1DOCBREAK",p)
+docbreak = re.sub(r"(.*NDA.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.*NR\&L.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.Am\. State Paper.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.*\[Statutes.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.*NYPL.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.*\[Treaties.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.*\[LC.*)",r"\1DOCBREAK",docbreak)
+docbreak = re.sub(r"(.*\[GAO.*)",r"\1DOCBREAK",docbreak)
+
 docbreaks = docbreak.split("DOCBREAK")
 
+#yielding one document at a time
 def snippetyielder(docbreaks):
 	for doc in docbreaks:
 		yield doc
- 			# elif re.match(r"(.*NDA.*)",line):
- 			# 	print line	
- 			# elif re.match(r"(.*NR\&L.*)",line):
- 			# 	print line
- 			# elif re.match(r"(.Am\. State Paper.*)",line):
- 			# 	print line
- 			# elif re.match(r"(.*\[Statutes.*)",line):
- 			# 	print line
- 			# elif re.match(r"(.*NYPL.*)",line):
- 			# 	print line
- 			# elif re.match(r"(.*\[Treaties.*)",line):
- 			# 	print line	
- 			# elif re.match(r"(.*\[LC.*)",line):
- 			# 	print line
- 			# elif re.match(r"(.*\[GAO.*)",line):
+
 
 generator = snippetyielder(docbreaks)
-test = generator.next()
 
-#testdoc = open('testdoc.txt', 'r')
+snippet = generator.next()
 
+#defining a class to pull out stuff from the snippets
 class Document():
 	def __init__(self, doc):
 		self.doc = doc
 
 	def raw_text(self, doc):
 		raw_text = re.sub(r"\f.*[0-9]+",r"",doc) #using formfeed to get rid of some page numbers/running heads
-		raw_text = re.sub(r"NAVAL OP.*",r"",doc) #eliminating more headers
- 		raw_text = re.sub(r"W.*B.*",r"",doc) #eliminating more headers
- 		raw_text = re.sub(r"(.*SDA.*)",r"",doc) #eliminating citations
+		raw_text = re.sub(r"NAVAL OP.*",r"",raw_text) #eliminating more headers
+ 		raw_text = re.sub(r"W.*B.*",r"",raw_text) #eliminating more headers
+ 		raw_text = re.sub(r"(.*SDA.*)",r"",raw_text) #eliminating citations
  		print raw_text
 
+ 	def author(self,doc):
+ 		author = re.search(r"[Ff]rom (\w \w)",doc)
+ 		print author
+
+
+# 	def title(self, doc):
+ #		title = 
 
 # def metadata:
 
@@ -51,4 +56,5 @@ class Document():
 # def does_this_look_suspicious:
 
 text = Document(test)
-print text.raw_text(test)
+for test in generator:
+	print text.author(test)
