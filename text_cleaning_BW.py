c55d9c71c61dcb9b0ed4b50170bdbcf46fb10272
Abby Mullen <abby@lincolnmullen.com>
2015-02-23 16:07:36 -0500

    Making progress on pulling out author and recipient

--- text_cleaning_BW.py
+++ text_cleaning_BW.py
@@ -1,33 +1,29 @@
 import re
 
-
-f = open("v1.txt", 'r')
-text = open('test.txt')
-a = text.readlines()
-p = "".join(a)
+def snippetyielder(filename):
+	text = open(filename, "r")
+	a = text.readlines()
+	p = "".join(a)
 
 #detecting the breaks between documents and identifying them to break the docs with
-docbreak = re.sub(r"(.*SDA.*)",r"\1DOCBREAK",p)
-docbreak = re.sub(r"(.*NDA.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.*NR\&L.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.Am\. State Paper.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.*\[Statutes.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.*NYPL.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.*\[Treaties.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.*\[LC.*)",r"\1DOCBREAK",docbreak)
-docbreak = re.sub(r"(.*\[GAO.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*SDA.*)",r"\1DOCBREAK",p)
+	docbreak = re.sub(r"(.*NDA.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*NR\&L.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.Am\. State Paper.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*\[Statutes.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*NYPL.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*\[Treaties.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*\[LC.*)",r"\1DOCBREAK",docbreak)
+	docbreak = re.sub(r"(.*\[GAO.*)",r"\1DOCBREAK",docbreak)
 
-docbreaks = docbreak.split("DOCBREAK")
+	docbreaks = docbreak.split("DOCBREAK")
 
 #yielding one document at a time
-def snippetyielder(docbreaks):
 	for doc in docbreaks:
 		yield doc
 
 
-generator = snippetyielder(docbreaks)
 
-snippet = generator.next()
 
 #defining a class to pull out stuff from the snippets
 class Document():
@@ -35,26 +31,49 @@ class Document():
 		self.doc = doc
 
 	def raw_text(self, doc):
+		#doc = str(doc)
 		raw_text = re.sub(r"\f.*[0-9]+",r"",doc) #using formfeed to get rid of some page numbers/running heads
 		raw_text = re.sub(r"NAVAL OP.*",r"",raw_text) #eliminating more headers
- 		raw_text = re.sub(r"W.*B.*",r"",raw_text) #eliminating more headers
- 		raw_text = re.sub(r"(.*SDA.*)",r"",raw_text) #eliminating citations
- 		print raw_text
-
- 	def author(self,doc):
- 		author = re.search(r"[Ff]rom (\w \w)",doc)
- 		print author
+		raw_text = re.sub(r"W.*B.*",r"",raw_text) #eliminating more headers
+		raw_text = re.sub(r"(.*SDA.*)",r"",raw_text) #eliminating citations
+		raw_text = re.sub(r"(.*NDA.*)",r"",raw_text) #eliminating citations
+		raw_text = re.sub(r"(.*NR\&L.*)",r"",raw_text) #eliminating citations
 
 
-# 	def title(self, doc):
- #		title = 
+		print raw_text
 
-# def metadata:
+	def author(self,doc):
+		author = re.search(r"(.*To)(.*)(from\s)(.*)",doc)
+		if author:
+			print author.group(4)
+		else:
+			print "Unknown"
 
-# def get_date:
+	def recipient(self, doc):
+		recipient = re.search(r"(To)(.*)([Ff]rom.*)",doc)
+		if recipient:
+			print recipient.group(2)
+		else:
+			print "Unknown"
 
-# def does_this_look_suspicious:
+ 	def title(self):
+ 		pass
+  
 
-text = Document(test)
-for test in generator:
-	print text.author(test)
+	def metadata(self):
+		pass
+
+	def get_date(self):
+		pass
+
+	def does_this_look_suspicious(self):
+		pass
+
+if __name__=="__main__":
+	generator = snippetyielder("test.txt")
+	for snippet in generator:
+		snippet = generator.next()
+		doc = Document(snippet)
+		print doc.author(snippet)
+	
+	
